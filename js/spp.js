e6a206e2e9f6e9798da52742284dfa1cffc967f7
Eabdalhaleem <eattaamneh@yahoo.com>
2021-05-11 23:55:31 +0300

    etharfile

---
+++ js/spp.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// question1
+let userName = prompt('what is your name?');
+console.log('what is your name ?'+ userName);
+alert('so your name is' + userName);
+
+// question2
+
+let userSize = prompt('what is your size ?');
+console.log('what is your size ?' + userSize);
+alert('so your name is ' + userName +'\n'+ 'so your size is '+ userSize);
+ 
